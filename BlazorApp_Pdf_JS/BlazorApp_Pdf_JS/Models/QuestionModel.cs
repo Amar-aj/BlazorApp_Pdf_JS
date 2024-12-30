86c56c5283c51e1de95b9af612b892e45a037403
Amar-aj <amar.aj.github@gmail.com>
2024-12-31 00:02:41 +0530

    js pdf and excel

---
+++ BlazorApp_Pdf_JS/BlazorApp_Pdf_JS/Models/QuestionModel.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorApp_Pdf_JS.Models;
+
+public class QuestionMSAModel
+{
+    public string Question { get; set; }
+    public string Option1 { get; set; }
+    public string Option2 { get; set; }
+    public string Option3 { get; set; }
+    public string Option4 { get; set; }
+    public string CorrectOption { get; set; }  
+    public int DurationInMinitus { get; set; }
+    public int Marks { get; set; }
+}
+public class QuestionTOFModel
+{
+    public string Question { get; set; }
+    public string Option1 { get; set; }
+    public string Option2 { get; set; }
+    public string CorrectOption { get; set; }  
+    public int DurationInMinitus { get; set; }
+    public int Marks { get; set; }
+}
